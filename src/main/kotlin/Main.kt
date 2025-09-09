92475411d3d165859a554bb522d61c1becabd34b
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2025-09-09 14:24:46 +0900

    ✨ 他ソフトに対応

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,22 @@
 package com.github.ringoame196
 
-fun main() {
-	val vrChatWindowName = "VRChat"
-	WindowManager.changeWindow(vrChatWindowName)
+fun main(args: Array<String>) {
+	val usage = "Usage: java -jar ScreenSizeChange.jar <windowName> <minimizeWidth> <minimizeHeight>"
+	if (args.size < 3) {
+		println(usage)
+		return
+	}
+
+	val windowName = args[0]
+	val minimizeWidth = args[1].toIntOrNull()
+	val minimizeHeight = args[2].toIntOrNull()
+
+	if (minimizeWidth == null || minimizeHeight == null) {
+		println(usage)
+		return
+	}
+
+	val windowManager = WindowManager(windowName, minimizeWidth, minimizeHeight)
+	windowManager.changeWindow()
 }
+
--- src/main/kotlin/WindowManager.kt
+++ src/main/kotlin/WindowManager.kt
@@ -3,42 +3,50 @@ package com.github.ringoame196
 import com.sun.jna.platform.win32.User32
 import com.sun.jna.platform.win32.WinDef
 
-object WindowManager {
-	const val SWP_NOZORDER = 0x0004
-	const val SWP_NOACTIVATE = 0x0010
-	const val MINIMIZE_WIDTH = 136
-	const val MINIMIZE_HEIGHT = 128
+class WindowManager(
+	val windowTitle: String,
+	val minimizeWidth: Int,
+	val minimizeHeight: Int
+) {
 
-	fun changeWindow(windowTitle: String) {
-		val hwnd = User32.INSTANCE.FindWindow(null, windowTitle)
+	fun changeWindow() {
+		val user32 = User32.INSTANCE
+		val hwnd = user32.FindWindow(null, windowTitle)
 		if (hwnd == null) {
 			println("ウインドウが見つかりません: $windowTitle")
 			return
 		}
+
 		// サイズ自動切り替え
-
 		if (isMinimize(hwnd)) {
-			User32.INSTANCE.ShowWindow(hwnd, User32.SW_MAXIMIZE)
+			// 最小化して最大化することで 変更を安定させる
+			user32.ShowWindow(hwnd, User32.SW_HIDE)
+			user32.ShowWindow(hwnd, User32.SW_MAXIMIZE)
+		} else {
+			// 指定サイズに変更
+			user32.SetWindowPos(
+				hwnd,
+				null,
+				0, 0,
+				minimizeWidth,
+				minimizeHeight,
+				Data.SWP_NOZORDER or Data.SWP_NOACTIVATE
+			)
 		}
-		else {
-			minimizeWindow(hwnd)
-		}
-		User32.INSTANCE.SetForegroundWindow(hwnd) // フォーカスも当てる
-		println("ウインドウのサイズを変更しました")
+
+		// フォーカスも当てる
+		user32.SetForegroundWindow(hwnd)
+
+		println("${windowTitle}のサイズを変更しました")
 	}
 
-	fun minimizeWindow(hwnd:WinDef.HWND) {
-		val user32 = User32.INSTANCE
-		// 位置 (X=0, Y=0) に 最小のサイズで移動
-		user32.SetWindowPos(hwnd, null, 0, 0, MINIMIZE_WIDTH, MINIMIZE_HEIGHT, SWP_NOZORDER or SWP_NOACTIVATE)
-	}
 
-	fun isMinimize(hwnd:WinDef.HWND): Boolean {
+	private fun isMinimize(hwnd:WinDef.HWND): Boolean {
 		val pair = getWindowSize(hwnd) ?: return false
-		return pair == Pair(MINIMIZE_WIDTH,MINIMIZE_HEIGHT)
+		return pair == Pair(minimizeWidth,minimizeHeight)
 	}
 
-	fun getWindowSize(hwnd:WinDef.HWND): Pair<Int, Int>? {
+	private fun getWindowSize(hwnd:WinDef.HWND): Pair<Int, Int>? {
 		val rect = WinDef.RECT()
 		if (User32.INSTANCE.GetWindowRect(hwnd, rect)) {
 			val width = rect.right - rect.left
