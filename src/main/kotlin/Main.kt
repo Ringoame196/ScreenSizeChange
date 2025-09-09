b1939b221c288c304e512430d36b6bb22746f34f
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2025-09-09 17:11:15 +0900

    ✨ batファイルを生成するように

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,19 @@
 package com.github.ringoame196
 
+import java.io.File
+
 fun main(args: Array<String>) {
 	val usage = "Usage: java -jar ScreenSizeChange.jar <windowName> (<minimizeWidth>) (<minimizeHeight>)"
 
 	if (args.size == 1) {
 		val windowTitle = args[0]
-		sendWindowSize(windowTitle)
-		println(usage)
+		val windowSize = WindowSizeHelper.getWindowSize(windowTitle)
+		if (windowSize != null) {
+			sendWindowSize(windowSize)
+			makeRunBatFile(windowTitle,windowSize)
+		} else {
+			println("ウィンドウが見つかりませんでした")
+		}
 		return
 	}
 
@@ -29,11 +36,22 @@ fun main(args: Array<String>) {
 	windowManager.changeWindow()
 }
 
-private fun sendWindowSize(windowName: String) {
-	val size = WindowSizeHelper.getWindowSize(windowName)
-	if (size != null) {
-		println("ウィンドウ '$windowName' の現在のサイズ: 幅=${size.width}, 高さ=${size.height}")
+private fun sendWindowSize(size: WindowSize) {
+	println("現在のサイズ: 幅=${size.width}, 高さ=${size.height}")
+}
+
+private fun makeRunBatFile(windowTitle:String,size: WindowSize) {
+	val jarName = getJarName()
+	if (jarName != null) {
+		val batFile = File("./run-${windowTitle}.bat")
+		batFile.writeText("java -jar $jarName $windowTitle ${size.width} ${size.height}")
+		println("起動用のbatを生成しました")
 	} else {
-		println("ウィンドウ '$windowName' が見つかりませんでした")
+		println("jarファイルの取得に失敗しました")
 	}
+}
+
+private fun getJarName(): String? {
+	val codeSource = object {}.javaClass.protectionDomain.codeSource
+	return codeSource?.location?.path?.substringAfterLast("/") // パスの最後を取得
 }
